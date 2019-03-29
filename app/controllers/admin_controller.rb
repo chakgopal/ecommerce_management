12d5dcc0c142f3653fd95dfe24acb1a176022f66
vinitakumari000 <vinitaksoni0502@gmail.com>
2019-03-29 16:12:54 +0530

    admin model

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,17 @@
+class AdminController < ApplicationController
+def index
+	render layout: false
+end
+
+def root
+  if admin_signed_in?
+    puts "coca-cola"
+    render "admin/index"
+  else
+    redirect_to new_admin_session_path
+  end
+
+end
+
+ end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  root to: 'seller#index'
+
+  root to: 'admin#index'
+  devise_for :admins, path:'', path_names: {sign_up: 'register', sign_in: 'login', sign_out: 'logout'} 
+  
   devise_for :sellers , path:'', path_names: {sign_up: 'register', sign_in: 'login', sign_out: 'logout'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
 end
+
