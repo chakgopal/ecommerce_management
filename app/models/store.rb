33cf63fb3380323d5bf29fff57a0899b9e0d5db3
gopal <chakgopal345@gmail.com>
2019-04-02 18:33:04 +0530

    added reference column and active storage

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,3 +1,12 @@
 class Store < ApplicationRecord
-  belongs_to :seller
+  belongs_to :seller ,required: false
+  
+  has_many_attached :shop_images
+  has_one_attached :gst_image
+  has_one_attached :shop_pan_image
+  has_one_attached :trade_license_image
+  has_one_attached :iso_image
+  has_one_attached :certificate_of_incorporation
+  has_one_attached :trademark_registration
+  has_one_attached :address_proff
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_26_122349) do
+ActiveRecord::Schema.define(version: 2019_04_02_062630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
 
   create_table "customer_addresses", force: :cascade do |t|
     t.string "address1"
@@ -38,19 +72,6 @@ ActiveRecord::Schema.define(version: 2019_03_26_122349) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
-  end
-
-  create_table "admins", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "name"
-    t.index ["email"], name: "index_admins_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
   create_table "inventory_stocks", force: :cascade do |t|
@@ -156,6 +177,10 @@ ActiveRecord::Schema.define(version: 2019_03_26_122349) do
     t.string "working_date_from"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "seller_id"
+    t.index ["seller_id"], name: "index_stores_on_seller_id"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "stores", "sellers"
 end
