13e79e9a74a5a19dd6e6595a1170adfeb1555718
vinitakumari000 <vinitaksoni0502@gmail.com>
2019-04-26 18:16:36 +0530

    added

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,32 +1,95 @@
 class QuotesController < ApplicationController
  
- def new
+ before_action :set_quote, only: [:show, :edit, :update, :destroy]
 
-  end 
-
-  def create
-   
-    logger.info current_customer.inspect
-    @order= Order.new(order_params.merge(customer_id: current_customer.id))
-    if @quote.save
-      render :plain => "Order generate successfully"
-      #redirect_to store_path
-    else
-      redirect_to 'new'
-    end
-   end
-  
+  # GET /quotes
+  # GET /quotes.json
   def index
-     @orders = Order.where(customer_id: current_customer.id)
+    @quotes = Quote.all
   end
 
+  # GET /quotes/1
+  # GET /quotes/1.json
+  def show
+    @quote = Quote.find(params[:id])
+  end
+
+  # GET /quotes/new
+  def new
+    name = params[:name]
+    short_desc = params[:short_desc]
+    shop_name = params[:shop_name]
+    price = params[:price]
+    sku = params[:sku]
+    id = params[:id] if params[:id].present?
+    puts price.to_s
+      if customer_signed_in? 
+        current_customer_id = current_customer.id
+        quote_count = Quote.where(customer_id: current_customer_id).count
+        if quote_count == 0
+          @quote = Quote.new
+        end
+      else
+        redirect_to new_customer_session_path
+      end    
+  end 
+
+  # GET /quotes/1/edit
+  def edit
+  end
+
+  # POST /quotes
+  # POST /quotes.json
+  def create
+    @quote = Quote.new(quote_params)
+
+    respond_to do |format|
+      if @quote.save
+        format.html { redirect_to @quote, notice: 'Quote was successfully created.' }
+        format.json { render :show, status: :created, location: @quote }
+      else
+        format.html { render :new }
+        format.json { render json: @quote.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /quotes/1
+  # PATCH/PUT /quotes/1.json
+  def update
+    respond_to do |format|
+      if @quote.update(quote_params)
+        format.html { redirect_to @quote, notice: 'Quote was successfully updated.' }
+        format.json { render :show, status: :ok, location: @quote }
+      else
+        format.html { render :edit }
+        format.json { render json: @quote.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /quotes/1
+  # DELETE /quotes/1.json
+  def destroy
+    @quote.destroy if @quote.id == session[:id]
+    session[:id] = nil
+    respond_to do |format|
+      format.html { redirect_to quotes_url, notice: 'Quote was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
 
   private
- def order_params
-    params.require(:order).permit(:)
- end
-end
+    # Use callbacks to share common setup or constraints between actions.
+    def set_quote
+      @quote = Quote.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def quote_params
+      params.fetch(:quote, {})
+    end
 
 
-
+  
 end
