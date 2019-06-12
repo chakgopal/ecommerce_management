8825a9654e7772d6ef2047d6a1b689060ccbcd53
gopal <chakgopal345@gmail.com>
2019-06-12 13:59:50 +0530

    stock reducing after order is placed is completed

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,16 @@
 class OrderItem < ApplicationRecord
+  after_save :change_inventory
+#for changing product stock after order is placed.
+  def change_inventory
+    order_item_obj = OrderItem.last
+    product_id = order_item_obj.product_id
+    inventory_of_ordered_product = InventoryStock.where(product_id:product_id)
+    quantity_in_inventory = inventory_of_ordered_product[0].quantity
+    quantity_in_order_item = order_item_obj.quantity
+    updated_quantity_in_inventory = quantity_in_inventory.to_i-quantity_in_order_item.to_i
+    InventoryStock.where(product_id:product_id).update(quantity: updated_quantity_in_inventory)
+  end
+
   belongs_to :order, required:false
   belongs_to :store, required:false
   belongs_to :product, required: false
