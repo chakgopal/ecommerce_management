4e4a7ae92f40ba22013b9bc2c691db7b0e79512c
gopal <chakgopal345@gmail.com>
2019-06-12 14:19:57 +0530

    change in reduce stock code

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -6,8 +6,8 @@ class OrderItem < ApplicationRecord
     product_id = order_item_obj.product_id
     inventory_of_ordered_product = InventoryStock.where(product_id:product_id)
     quantity_in_inventory = inventory_of_ordered_product[0].quantity
-    quantity_in_order_item = order_item_obj.quantity
-    updated_quantity_in_inventory = quantity_in_inventory.to_i-quantity_in_order_item.to_i
+    quantity_in_ordered_item = order_item_obj.quantity
+    updated_quantity_in_inventory = quantity_in_inventory.to_i-quantity_in_ordered_item.to_i
     InventoryStock.where(product_id:product_id).update(quantity: updated_quantity_in_inventory)
   end
 
